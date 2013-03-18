9c84ab9aa43f467aee0202855a5cb3d5cd867f79
Ruggero Russo <ruggero.russo89@gmail.com>
2013-03-18 18:21:26 +0100

    Cancellata modifica precedente

--- main.cpp
+++ main.cpp
@@ -6,11 +6,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    QRect screenGeometry = QApplication::desktop()->screenGeometry();
-    int x = (screenGeometry.width()-w.width()) / 2;
-    int y = (screenGeometry.height()-w.height()) / 2;
-    w.move(x, y);
-    //w.show();
+    w.show();
     
     return a.exec();
 }
